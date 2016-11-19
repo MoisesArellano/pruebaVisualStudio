990ec649a53fd608b38bdeeaa977fe040a958f54
lopezMaria <fernandalm45@hotmail.com>
2016-11-19 15:45:18 -0600

    Se agregaron los diseños de las letras

---
+++ EyeComm/EyeComm/Diseños/LetrasK-Ñ.Designer.cs
@@ -0,0 +1,290 @@
+﻿namespace EyeComm.Diseños
+{
+    partial class LetrasK_Ñ
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
+            this.tableLayoutPanel2 = new System.Windows.Forms.TableLayoutPanel();
+            this.LetraN = new System.Windows.Forms.Button();
+            this.LetraM = new System.Windows.Forms.Button();
+            this.LetraK = new System.Windows.Forms.Button();
+            this.LetraL = new System.Windows.Forms.Button();
+            this.LetraÑ = new System.Windows.Forms.Button();
+            this.flowLayoutPanel1 = new System.Windows.Forms.FlowLayoutPanel();
+            this.txtFJ = new System.Windows.Forms.TextBox();
+            this.tableLayoutPanel3 = new System.Windows.Forms.TableLayoutPanel();
+            this.btnBorrar = new System.Windows.Forms.Button();
+            this.btnBorrarPalabra = new System.Windows.Forms.Button();
+            this.btnVoz = new System.Windows.Forms.Button();
+            this.btnRegresar = new System.Windows.Forms.Button();
+            this.tableLayoutPanel1.SuspendLayout();
+            this.tableLayoutPanel2.SuspendLayout();
+            this.flowLayoutPanel1.SuspendLayout();
+            this.tableLayoutPanel3.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // tableLayoutPanel1
+            // 
+            this.tableLayoutPanel1.ColumnCount = 1;
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 100F));
+            this.tableLayoutPanel1.Controls.Add(this.tableLayoutPanel2, 0, 1);
+            this.tableLayoutPanel1.Controls.Add(this.flowLayoutPanel1, 0, 0);
+            this.tableLayoutPanel1.Controls.Add(this.tableLayoutPanel3, 0, 2);
+            this.tableLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tableLayoutPanel1.Location = new System.Drawing.Point(0, 0);
+            this.tableLayoutPanel1.Name = "tableLayoutPanel1";
+            this.tableLayoutPanel1.RowCount = 3;
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 121F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel1.Size = new System.Drawing.Size(664, 380);
+            this.tableLayoutPanel1.TabIndex = 4;
+            // 
+            // tableLayoutPanel2
+            // 
+            this.tableLayoutPanel2.ColumnCount = 5;
+            this.tableLayoutPanel2.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 20F));
+            this.tableLayoutPanel2.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 20F));
+            this.tableLayoutPanel2.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 20F));
+            this.tableLayoutPanel2.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 20F));
+            this.tableLayoutPanel2.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 20F));
+            this.tableLayoutPanel2.Controls.Add(this.LetraN, 3, 0);
+            this.tableLayoutPanel2.Controls.Add(this.LetraM, 2, 0);
+            this.tableLayoutPanel2.Controls.Add(this.LetraK, 0, 0);
+            this.tableLayoutPanel2.Controls.Add(this.LetraL, 1, 0);
+            this.tableLayoutPanel2.Controls.Add(this.LetraÑ, 4, 0);
+            this.tableLayoutPanel2.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tableLayoutPanel2.Location = new System.Drawing.Point(3, 124);
+            this.tableLayoutPanel2.Name = "tableLayoutPanel2";
+            this.tableLayoutPanel2.RowCount = 1;
+            this.tableLayoutPanel2.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 100F));
+            this.tableLayoutPanel2.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 123F));
+            this.tableLayoutPanel2.Size = new System.Drawing.Size(658, 123);
+            this.tableLayoutPanel2.TabIndex = 0;
+            // 
+            // LetraN
+            // 
+            this.LetraN.BackColor = System.Drawing.Color.White;
+            this.LetraN.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.LetraN.FlatAppearance.BorderSize = 3;
+            this.LetraN.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.LetraN.Font = new System.Drawing.Font("Microsoft Sans Serif", 30F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.LetraN.Location = new System.Drawing.Point(396, 3);
+            this.LetraN.Name = "LetraN";
+            this.LetraN.Size = new System.Drawing.Size(125, 117);
+            this.LetraN.TabIndex = 3;
+            this.LetraN.Text = "N";
+            this.LetraN.UseVisualStyleBackColor = false;
+            // 
+            // LetraM
+            // 
+            this.LetraM.BackColor = System.Drawing.Color.White;
+            this.LetraM.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.LetraM.FlatAppearance.BorderSize = 3;
+            this.LetraM.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.LetraM.Font = new System.Drawing.Font("Microsoft Sans Serif", 30F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.LetraM.Location = new System.Drawing.Point(265, 3);
+            this.LetraM.Name = "LetraM";
+            this.LetraM.Size = new System.Drawing.Size(125, 117);
+            this.LetraM.TabIndex = 2;
+            this.LetraM.Text = "M";
+            this.LetraM.UseVisualStyleBackColor = false;
+            // 
+            // LetraK
+            // 
+            this.LetraK.BackColor = System.Drawing.Color.White;
+            this.LetraK.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.LetraK.FlatAppearance.BorderSize = 3;
+            this.LetraK.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.LetraK.Font = new System.Drawing.Font("Microsoft Sans Serif", 30F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.LetraK.Location = new System.Drawing.Point(3, 3);
+            this.LetraK.Name = "LetraK";
+            this.LetraK.Size = new System.Drawing.Size(125, 117);
+            this.LetraK.TabIndex = 1;
+            this.LetraK.TabStop = false;
+            this.LetraK.Text = "K";
+            this.LetraK.UseVisualStyleBackColor = false;
+            // 
+            // LetraL
+            // 
+            this.LetraL.BackColor = System.Drawing.Color.White;
+            this.LetraL.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.LetraL.FlatAppearance.BorderSize = 3;
+            this.LetraL.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.LetraL.Font = new System.Drawing.Font("Microsoft Sans Serif", 30F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.LetraL.Location = new System.Drawing.Point(134, 3);
+            this.LetraL.Name = "LetraL";
+            this.LetraL.Size = new System.Drawing.Size(125, 117);
+            this.LetraL.TabIndex = 0;
+            this.LetraL.Text = "L";
+            this.LetraL.UseVisualStyleBackColor = false;
+            // 
+            // LetraÑ
+            // 
+            this.LetraÑ.BackColor = System.Drawing.Color.White;
+            this.LetraÑ.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.LetraÑ.FlatAppearance.BorderSize = 3;
+            this.LetraÑ.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.LetraÑ.Font = new System.Drawing.Font("Microsoft Sans Serif", 30F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.LetraÑ.Location = new System.Drawing.Point(527, 3);
+            this.LetraÑ.Name = "LetraÑ";
+            this.LetraÑ.Size = new System.Drawing.Size(128, 117);
+            this.LetraÑ.TabIndex = 9;
+            this.LetraÑ.Text = "Ñ";
+            this.LetraÑ.UseVisualStyleBackColor = false;
+            // 
+            // flowLayoutPanel1
+            // 
+            this.flowLayoutPanel1.Controls.Add(this.txtFJ);
+            this.flowLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.flowLayoutPanel1.Location = new System.Drawing.Point(3, 3);
+            this.flowLayoutPanel1.Name = "flowLayoutPanel1";
+            this.flowLayoutPanel1.Size = new System.Drawing.Size(658, 115);
+            this.flowLayoutPanel1.TabIndex = 1;
+            // 
+            // txtFJ
+            // 
+            this.txtFJ.Location = new System.Drawing.Point(3, 3);
+            this.txtFJ.Multiline = true;
+            this.txtFJ.Name = "txtFJ";
+            this.txtFJ.Size = new System.Drawing.Size(653, 100);
+            this.txtFJ.TabIndex = 0;
+            // 
+            // tableLayoutPanel3
+            // 
+            this.tableLayoutPanel3.ColumnCount = 4;
+            this.tableLayoutPanel3.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 25F));
+            this.tableLayoutPanel3.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 25F));
+            this.tableLayoutPanel3.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 25F));
+            this.tableLayoutPanel3.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 25F));
+            this.tableLayoutPanel3.Controls.Add(this.btnBorrar, 0, 0);
+            this.tableLayoutPanel3.Controls.Add(this.btnBorrarPalabra, 1, 0);
+            this.tableLayoutPanel3.Controls.Add(this.btnVoz, 2, 0);
+            this.tableLayoutPanel3.Controls.Add(this.btnRegresar, 3, 0);
+            this.tableLayoutPanel3.Location = new System.Drawing.Point(3, 253);
+            this.tableLayoutPanel3.Name = "tableLayoutPanel3";
+            this.tableLayoutPanel3.RowCount = 1;
+            this.tableLayoutPanel3.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 100F));
+            this.tableLayoutPanel3.Size = new System.Drawing.Size(658, 124);
+            this.tableLayoutPanel3.TabIndex = 2;
+            // 
+            // btnBorrar
+            // 
+            this.btnBorrar.BackColor = System.Drawing.Color.White;
+            this.btnBorrar.BackgroundImage = global::EyeComm.Properties.Resources.Typicons_e026_0__256;
+            this.btnBorrar.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.btnBorrar.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.btnBorrar.FlatAppearance.BorderSize = 3;
+            this.btnBorrar.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnBorrar.Font = new System.Drawing.Font("Microsoft Sans Serif", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnBorrar.Location = new System.Drawing.Point(3, 3);
+            this.btnBorrar.Name = "btnBorrar";
+            this.btnBorrar.Size = new System.Drawing.Size(158, 118);
+            this.btnBorrar.TabIndex = 0;
+            this.btnBorrar.UseVisualStyleBackColor = false;
+            // 
+            // btnBorrarPalabra
+            // 
+            this.btnBorrarPalabra.BackColor = System.Drawing.Color.White;
+            this.btnBorrarPalabra.BackgroundImage = global::EyeComm.Properties.Resources.linecons_e006_0__128;
+            this.btnBorrarPalabra.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.btnBorrarPalabra.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.btnBorrarPalabra.FlatAppearance.BorderSize = 3;
+            this.btnBorrarPalabra.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnBorrarPalabra.Font = new System.Drawing.Font("Microsoft Sans Serif", 48F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnBorrarPalabra.Location = new System.Drawing.Point(167, 3);
+            this.btnBorrarPalabra.Name = "btnBorrarPalabra";
+            this.btnBorrarPalabra.Size = new System.Drawing.Size(158, 118);
+            this.btnBorrarPalabra.TabIndex = 1;
+            this.btnBorrarPalabra.UseVisualStyleBackColor = false;
+            // 
+            // btnVoz
+            // 
+            this.btnVoz.BackColor = System.Drawing.Color.White;
+            this.btnVoz.BackgroundImage = global::EyeComm.Properties.Resources.bocina1;
+            this.btnVoz.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.btnVoz.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.btnVoz.FlatAppearance.BorderSize = 3;
+            this.btnVoz.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnVoz.Font = new System.Drawing.Font("Microsoft Sans Serif", 30F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnVoz.Location = new System.Drawing.Point(331, 3);
+            this.btnVoz.Name = "btnVoz";
+            this.btnVoz.Size = new System.Drawing.Size(158, 118);
+            this.btnVoz.TabIndex = 2;
+            this.btnVoz.UseVisualStyleBackColor = false;
+            // 
+            // btnRegresar
+            // 
+            this.btnRegresar.BackColor = System.Drawing.Color.White;
+            this.btnRegresar.BackgroundImage = global::EyeComm.Properties.Resources.Typicons_e006_1__256;
+            this.btnRegresar.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.btnRegresar.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.btnRegresar.FlatAppearance.BorderSize = 3;
+            this.btnRegresar.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnRegresar.Font = new System.Drawing.Font("Microsoft Sans Serif", 30F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnRegresar.Location = new System.Drawing.Point(495, 3);
+            this.btnRegresar.Name = "btnRegresar";
+            this.btnRegresar.Size = new System.Drawing.Size(160, 118);
+            this.btnRegresar.TabIndex = 3;
+            this.btnRegresar.UseVisualStyleBackColor = false;
+            // 
+            // LetrasK_Ñ
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(664, 380);
+            this.Controls.Add(this.tableLayoutPanel1);
+            this.Name = "LetrasK_Ñ";
+            this.Text = "LetrasK_Ñ";
+            this.tableLayoutPanel1.ResumeLayout(false);
+            this.tableLayoutPanel2.ResumeLayout(false);
+            this.flowLayoutPanel1.ResumeLayout(false);
+            this.flowLayoutPanel1.PerformLayout();
+            this.tableLayoutPanel3.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel2;
+        private System.Windows.Forms.Button LetraN;
+        private System.Windows.Forms.Button LetraM;
+        private System.Windows.Forms.Button LetraK;
+        private System.Windows.Forms.Button LetraL;
+        private System.Windows.Forms.Button LetraÑ;
+        private System.Windows.Forms.FlowLayoutPanel flowLayoutPanel1;
+        private System.Windows.Forms.TextBox txtFJ;
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel3;
+        private System.Windows.Forms.Button btnBorrar;
+        private System.Windows.Forms.Button btnBorrarPalabra;
+        private System.Windows.Forms.Button btnVoz;
+        private System.Windows.Forms.Button btnRegresar;
+    }
+}
